b4570b93fabb65ab8d86af195caf4e1f4042e49f
Alejandro Jimenez <alejandro.jimenez@desarrollosnea.com>
2020-08-21 00:01:16 -0300

    [TP1]Mi primer formulario

--- Formulario1/Practico1/Formulario1.Designer.cs
+++ Formulario1/Practico1/Formulario1.Designer.cs
@@ -28,33 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.textMultilinea = new System.Windows.Forms.TextBox();
+            this.BSalir = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // BGuardar
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(94, 68);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
+            this.BGuardar.Location = new System.Drawing.Point(44, 179);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BGuardar.TabIndex = 0;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(135, 179);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BEliminar.TabIndex = 1;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(41, 57);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(41, 97);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 3;
+            this.LNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 57);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(91, 97);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // textMultilinea
+            // 
+            this.textMultilinea.Location = new System.Drawing.Point(262, 54);
+            this.textMultilinea.Multiline = true;
+            this.textMultilinea.Name = "textMultilinea";
+            this.textMultilinea.Size = new System.Drawing.Size(196, 148);
+            this.textMultilinea.TabIndex = 6;
+            // 
+            // BSalir
+            // 
+            this.BSalir.Location = new System.Drawing.Point(330, 208);
+            this.BSalir.Name = "BSalir";
+            this.BSalir.Size = new System.Drawing.Size(75, 23);
+            this.BSalir.TabIndex = 7;
+            this.BSalir.Text = "Salir";
+            this.BSalir.UseVisualStyleBackColor = true;
+            this.BSalir.Visible = false;
+            this.BSalir.Click += new System.EventHandler(this.BSalir_Click);
             // 
             // Formulario1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 261);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.BSalir);
+            this.Controls.Add(this.textMultilinea);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.KeyPreview = true;
             this.Name = "Formulario1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mi Primer Forms";
             this.Load += new System.EventHandler(this.Formulario1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Formulario1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox textMultilinea;
+        private System.Windows.Forms.Button BSalir;
     }
 }
 
--- Formulario1/Practico1/Formulario1.cs
+++ Formulario1/Practico1/Formulario1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,63 @@ namespace Practico1
 
         private void Formulario1_Load(object sender, EventArgs e)
         {
-
+           
         }
+
+        #region Eventos
+        private void BGuardar_Click(object sender, EventArgs e)
+        {
+            var result = Concatenar(this.txtNombre.Text, this.txtApellido.Text);
+            this.textMultilinea.Text = result;
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+        /// <summary>
+        /// Realiza una accion al presionar una(as) tecla(as)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">obtejo de tipo KeyEventArgs</param>
+        private void Formulario1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //KeyData : obtejo de tipo enun que posee todas las teclas.
+            if (e.KeyData == (Keys.Control | Keys.S))
+            {
+                this.BSalir.Visible = true; 
+                this.BSalir.Focus();
+                
+            }
+        }
+
+        private void BSalir_Click(object sender, EventArgs e)
+        {
+            Salir();
+        }
+        #endregion
+
+        #region Metodos Privados
+
+        private string Concatenar(string nombre, string apellido)
+        {
+            var result = $"{nombre} {apellido}";
+            return result;
+        }
+
+        private void Limpiar()
+        {
+            this.textMultilinea.Clear();
+        }
+
+        private void Salir()
+        {
+            //TODO: Idem a VB -> End
+            this.Close();
+        }
+
+        #endregion
+
+
     }
 }
